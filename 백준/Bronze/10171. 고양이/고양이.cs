936cddfe8ca9859addefaf04a9fed0501744279c
Han soo <103415740+shimhansoo@users.noreply.github.com>
2023-03-16 10:48:20 +0900

    [Bronze V] Title: 고양이, Time: 60 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Algorithm
+{
+    static void Main()
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+}
