d3eda13175fa6dd3149e7b70060e4a61e564ace1
Han soo <103415740+shimhansoo@users.noreply.github.com>
2023-03-16 11:30:25 +0900

    [Bronze V] Title: 개, Time: 64 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Algorithm
+{
+    static void Main()
+    {
+        Console.WriteLine("|\\_/|");
+        Console.WriteLine("|q p|   /}");
+        Console.WriteLine("( 0 )\"\"\"\\");
+        Console.WriteLine("|\"^\"`    |");
+        Console.WriteLine("||_/=\\\\__|");
+    }
+}
