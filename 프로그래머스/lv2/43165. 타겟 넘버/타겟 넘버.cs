f071261773d665896e9b7b32ec0a61ed329f25c7
Han soo <103415740+shimhansoo@users.noreply.github.com>
2023-04-09 09:58:45 +0900

    [level 2] Title: 타겟 넘버, Time: 0.27 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers, int target) {
+        return Dfs(numbers, target, 0, 0);
+    }
+    
+    static int Dfs(int[] arr, int target, int idx, int num)
+    {
+        if (idx == arr.Length)
+        {
+            if (target == num) return 1;
+            else return 0;
+        }
+        else
+            return Dfs(arr, target, idx + 1, num + arr[idx]) +
+            Dfs(arr, target, idx + 1, num - arr[idx]);
+    }
+
+}
