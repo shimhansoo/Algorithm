69bec37010c79e3a29054052dc71ca1f9371aeab
Han soo <103415740+shimhansoo@users.noreply.github.com>
2023-04-12 21:16:26 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.12 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[,] solution(int[,] arr1, int[,] arr2) {
+	  
+        int[,] answer = new int[arr1.GetLength(0), arr2.GetLength(1)];
+	
+        for (int i = 0; i < answer.GetLength(0); i++) {
+            for (int j = 0; j < answer.GetLength(1); j++) {
+                for (int k = 0; k < arr1.GetLength(1); k++) {
+                    answer[i, j] += arr1[i, k] * arr2[k, j];
+                }
+            }
+        }
+        return answer;
+    }
+}
