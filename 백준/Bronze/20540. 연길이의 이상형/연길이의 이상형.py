323ee6509678d97a5662d158143dabdb4a26e2ed
Han soo <103415740+shimhansoo@users.noreply.github.com>
2023-05-26 15:00:13 +0900

    [Bronze III] Title: 연길이의 이상형, Time: 64 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.py
@@ -0,0 +1,20 @@
+MBTI = [0,0,0,0]
+inputA = input()
+B = list(inputA)
+
+if B[0] == "E":
+    print("I", end="")
+elif B[0] == "I":
+    print("E", end="")
+if B[1] == "N":
+    print("S", end="")
+elif B[1] == "S":
+    print("N", end="")
+if B[2] == "F":
+    print("T", end="")
+elif B[2] == "T":
+    print("F", end="")
+if B[3] == "J":
+    print("P", end="")
+elif B[3] == "P":
+    print("J", end="")
